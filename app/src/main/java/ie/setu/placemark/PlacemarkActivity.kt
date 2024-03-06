d2ee6cf523560af3b9a123afce45e0a7ce920451
brendanoc21 <brendanoconnor2030@gmail.com>
2024-03-06 16:18:44 +0000

    Add Theme change button

--- app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
@@ -2,15 +2,34 @@ package ie.setu.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import ie.setu.placemark.databinding.ActivityPlacemarkBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlacemarkBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
 
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Timber.plant(Timber.DebugTree())
         i("Placemark Activity started..")
+
+        binding.themeButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                Toast.makeText(applicationContext, "Dark Mode", Toast.LENGTH_LONG).show()
+                i("Dark Mode active")
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Toast.makeText(applicationContext, "Light Mode", Toast.LENGTH_LONG).show()
+                i("Light mode active")
+            }
+        }
     }
 }
