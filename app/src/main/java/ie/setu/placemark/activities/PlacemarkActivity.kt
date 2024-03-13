9a79361e6a605fb0474bc61edac0eb3b6b75ce57
brendanoc21 <brendanoconnor2030@gmail.com>
2024-03-13 16:09:54 +0000

    Add Placemark 01 additions

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -1,25 +1,46 @@
-package ie.setu.placemark
+package ie.setu.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.snackbar.Snackbar
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
+import ie.setu.placemark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
+    val placemarks = ArrayList<PlacemarkModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placemark)
 
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started..")
 
+        i("Placemark Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            placemark.title = binding.placemarkTitle.text.toString()
+            placemark.description = binding.placemarkDescription.text.toString()
+            if (placemark.title.isNotEmpty() && placemark.description.isNotEmpty()) {
+                i("add Button Pressed: ${placemark.title}, ${placemark.description}")
+                placemarks.add(placemark.copy())
+                i("${placemarks}")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title and description", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+}
+        /*
         binding.themeButton.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -31,5 +52,4 @@ class PlacemarkActivity : AppCompatActivity() {
                 i("Light mode active")
             }
         }
-    }
-}
+        */
