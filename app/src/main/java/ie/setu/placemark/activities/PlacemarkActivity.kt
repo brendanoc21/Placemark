0c9bb1d8e37e1318361bb7beb74b62d32f86cd3c
brendanoc21 <brendanoconnor2030@gmail.com>
2024-03-16 19:31:28 +0000

    Add completed aspects of Placemark 02

--- app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkActivity.kt
@@ -2,7 +2,10 @@ package ie.setu.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import ie.setu.placemark.R
 import ie.setu.placemark.databinding.ActivityPlacemarkBinding
 import ie.setu.placemark.main.MainApp
 import ie.setu.placemark.models.PlacemarkModel
@@ -21,37 +24,43 @@ class PlacemarkActivity : AppCompatActivity() {
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.topAppBar.title = title
+        setSupportActionBar(binding.topAppBar)
+
         app = application as MainApp
         i("Placemark Activity started...")
 
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.placemarkDescription.text.toString()
-            if (placemark.title.isNotEmpty() && placemark.description.isNotEmpty()) {
-                i("add Button Pressed: ${placemark.title}, ${placemark.description}")
+            if (placemark.title.isNotEmpty()) {
                 app.placemarks.add(placemark.copy())
+                i("add Button Pressed: ${placemark}")
                 for (i in app.placemarks.indices) {
-                    i("Placemark[$i]:${app.placemarks[i]}")
+                    i("Placemark[$i]:${this.app.placemarks[i]}")
                 }
+                setResult(RESULT_OK)
+                finish()
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a title and description", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
     }
-}
-        /*
-        binding.themeButton.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                Toast.makeText(applicationContext, "Dark Mode", Toast.LENGTH_LONG).show()
-                i("Dark Mode active")
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                Toast.makeText(applicationContext, "Light Mode", Toast.LENGTH_LONG).show()
-                i("Light mode active")
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_placemark, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                setResult(RESULT_CANCELED)
+                finish()
             }
         }
-        */
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/ie/setu/placemark/activities/PlacemarkListActivity.kt
@@ -0,0 +1,64 @@
+package ie.setu.placemark.activities
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.placemark.R
+import ie.setu.placemark.adapters.PlacemarkAdapter
+import ie.setu.placemark.databinding.ActivityPlacemarkListBinding
+import ie.setu.placemark.main.MainApp
+
+class PlacemarkListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+    private lateinit var binding: ActivityPlacemarkListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPlacemarkListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.title = title
+        setSupportActionBar(binding.topAppBar)
+
+        app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> {
+                val launcherIntent = Intent(this, PlacemarkActivity::class.java)
+                getResult.launch(launcherIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                (binding.recyclerView.adapter)?.
+                notifyItemRangeChanged(0,app.placemarks.size)
+            }
+            if (it.resultCode == Activity.RESULT_CANCELED) {
+                Snackbar.make(binding.root, "Placemark Add Cancelled", Snackbar.LENGTH_LONG).show()
+            }
+        }
+}
